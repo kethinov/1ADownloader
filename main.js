5e3216116dbe02835ba52970b1422f0a574b7a27
Eric Newport <kethinov@gmail.com>
2018-03-28 00:02:22 -0400

    1.0.3

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-const electron = require('electron'),
-      {app} = electron,
-      {BrowserWindow} = electron,
-      {ipcMain} = electron;
+const electron = require('electron')
+const path = require('path')
+const {app} = electron
+const {BrowserWindow} = electron
+const {ipcMain} = electron
 
-app.on('ready', function() {
+app.on('ready', function () {
   let win = new BrowserWindow({
     width: 600,
     height: 900,
-    icon: process.platform === 'linux' && __dirname + '/images/icon.png'
-  });
-  win.loadURL('file://' + __dirname + '/index.html');
+    icon: process.platform === 'linux' && path.join(__dirname, '/images/icon.png')
+  })
+  win.loadURL('file://' + path.join(__dirname, '/index.html'))
   // win.openDevTools(); // uncomment to enter dev mode
 
-  win.on('closed', function() {
-    win = null;
-  });
+  win.on('closed', function () {
+    win = null
+  })
 
-  ipcMain.on('openListenWindow', function(event, data) {
+  ipcMain.on('openListenWindow', function (event, data) {
     let listenWindow = new BrowserWindow({
       width: 320,
       height: 100,
       title: '1A - ' + data.epTitle
-    });
-    listenWindow.loadURL(data.href);
-    listenWindow.on('closed', function() {
-      listenWindow = null;
-    });
-  });
-});
+    })
+    listenWindow.loadURL(data.href)
+    listenWindow.on('closed', function () {
+      listenWindow = null
+    })
+  })
+})
 
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
